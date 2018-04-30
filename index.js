1ad50986d36f8e23c13be20d75c944f4392d9bbc
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-05-01 00:12:20 +0100

    sprites and background

--- index.js
+++ index.js
@@ -1,84 +1,163 @@
 
 var startId = 25;
 var currentPosition = $('.box');
-var endId = $('#275');
+// var endId = $('#275');
+var sword = false;
+var encounter = false;
+var shield = false;
+var health = false;
+var haveKey = false;
+var swordMult = 1;
+var shieldMult = 1;
+var healthTotal = 10;
+
 
 
 $(document).keydown(function(e) {
-
+		// gameEnd();
     switch(e.which) {
 
         case 37: // left
-
-				var newId = startId - 1;
-				var nextPosition = $('#'+newId);
-				if (nextPosition.hasClass('wall')){
-					return;
-				} else {
-				$(nextPosition).addClass('box');
-				$(currentPosition).removeClass('box');
-				currentPosition = nextPosition;
-				startId = newId;
-				}
+				moveLeft();
         break;
 
         case 38: // up
-
-
-				var newId = startId - 23;
-				var nextPosition = $('#'+newId);
-				if (nextPosition.hasClass('wall')){
-					return;
-				} else {
-				$(nextPosition).addClass('box');
-				$(currentPosition).removeClass('box');
-				currentPosition = nextPosition;
-				startId = newId;
-				}
-
+				moveUp();
         break;
 
         case 39: // right
-
-				var newId = startId + 1;
-				var nextPosition = $('#'+newId);
-				if (nextPosition.hasClass('wall')){
-					return;
-				} else {
-				$(nextPosition).addClass('box');
-				$(currentPosition).removeClass('box');
-				currentPosition = nextPosition;
-				startId = newId;
-				}
+				moveRight();
         break;
 
         case 40: // down
-
-				var newId = startId + 23;
-				var nextPosition = $('#'+newId);
-				if (nextPosition.hasClass('wall')){
-					return;
-				} else {
-				$(nextPosition).addClass('box');
-				$(currentPosition).removeClass('box');
-				gameEnd();
-				currentPosition = nextPosition;
-				startId = newId;
-				}
+				moveDown();
         break;
+
+				case 70:
+				fKey();
+				break;
 
         default: return; // exit for other keys
     }
-		// gameEnd();
+
 		e.preventDefault(); // prevent the default action (scroll / move caret)
 });
 
-function gameEnd() {
-	if (endId.hasClass('box')){
-		alert("END!");
+//Left arrow
+function moveLeft (){
+	var newId = startId - 1;
+	var nextPosition = $('#'+newId);
+	if (nextPosition.hasClass('wall')){
+		return;
+	} else {
+	$(nextPosition).addClass('box');
+	$(currentPosition).removeClass('box');
+	currentPosition = nextPosition;
+	startId = newId;
 	}
 }
 
-function gameTime(){
-	
+//Up arrow
+function moveUp(){
+	var newId = startId - 23;
+	var nextPosition = $('#'+newId);
+	if (nextPosition.hasClass('wall')){
+		return;
+	} else {
+	$(nextPosition).addClass('box');
+	$(currentPosition).removeClass('box');
+	currentPosition = nextPosition;
+	startId = newId;
+	}
 }
+
+//Right arrow
+function moveRight(){
+	var newId = startId + 1;
+	var nextPosition = $('#'+newId);
+	if (nextPosition.hasClass('wall')){
+		return;
+	} else {
+	$(nextPosition).addClass('box');
+	$(currentPosition).removeClass('box');
+	currentPosition = nextPosition;
+	startId = newId;
+	}
+}
+
+//Down arrow
+function moveDown(){
+	var newId = startId + 23;
+	var nextPosition = $('#'+newId);
+	if (nextPosition.hasClass('wall')){
+		return;
+	} else {
+	$(nextPosition).addClass('box');
+	$(currentPosition).removeClass('box');
+	currentPosition = nextPosition;
+	startId = newId;
+	}
+}
+
+//F key/interract
+function fKey(){
+	if (currentPosition.hasClass('chest')){
+		chestItem();
+	} if (currentPosition.hasClass('door')){
+		doorOpen();
+	}
+}
+
+function chestItem(){
+	if (currentPosition.is('#29')|| currentPosition.is("#38")){
+		sword = true;
+		console.log("You've found a sword, Your attack is increased +0.2!");
+		currentPosition.removeClass('chest');
+		swordMult = swordMult + 0.2;
+		console.log(swordMult);
+		currentPosition.addClass('chestSprite2')
+	} if (currentPosition.is('#74')|| currentPosition.is("#264")){
+		encounter = true;
+		console.log("Enemy encountered!!");
+		currentPosition.removeClass('chest');
+		currentPosition.addClass('chestSprite2')
+	} if (currentPosition.is('#256')|| currentPosition.is("#250")){
+		shield = true;
+		console.log("You've found a shield, damage you take is reduced by +0.2!");
+		currentPosition.removeClass('chest');
+		shieldMult = shieldMult + 0.2;
+		console.log(shieldMult);
+		currentPosition.addClass('chestSprite2')
+	} if (currentPosition.is('#174')|| currentPosition.is("#135")){
+		health = true;
+		console.log("You've found a health potion, your health has increased by 20%");
+		currentPosition.removeClass('chest');
+		healthTotal = healthTotal + 2;
+		console.log(healthTotal);
+		currentPosition.addClass('chestSprite2')
+	} if (currentPosition.is("#213")){
+		haveKey = true;
+		console.log("You've found a Key, You can now open the door!");
+		currentPosition.removeClass('chest');
+		currentPosition.addClass('chestSprite2')
+}
+}
+//End game alert
+// function gameEnd() {
+// 	if (endId.hasClass('box')){
+// 		alert("END!");
+// 	}
+// }
+
+//timer
+// var seconds_left = 30;
+// var interval = setInterval(function() {
+//     document.getElementById('timer_div').innerHTML = "Time remaining: " + --seconds_left;
+//
+//     if (seconds_left <= 0)
+//     {
+//         document.getElementById('timer_div').innerHTML = 'You Lose!';
+// 				alert("You Lose!")
+//         clearInterval(interval);
+//     }
+// }, 1000);
