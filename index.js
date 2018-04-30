9ee635aed6762ffcacde2faa1d20f1e65a74b315
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-04-30 16:03:27 +0100

    reshuffled ids of grid

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 
-var row1 = $('.r1');
-var row2 = $('.r2');
-var row3 = $('.r3');
-var row4 = $('.r4');
-var row5 = $('.r5');
-var row6 = $('.r6');
-var col1 = $('.c1');
-var col2 = $('.c2');
-var col3 = $('.c3');
-var col4 = $('.c4');
-var col5 = $('.c5');
-var col6 = $('.c6');
-var rows = [row1, row2, row3, row4, row5, row6];
-var cols = [col1, col2, col3, col4, col5, col6];
-
-var startId = 23
+var startId = 25;
 var currentPosition = $('.box');
+var endId = $('#275');
+
 
 $(document).keydown(function(e) {
 
     switch(e.which) {
+
         case 37: // left
 
 				var newId = startId - 1;
@@ -36,6 +24,7 @@ $(document).keydown(function(e) {
 
         case 38: // up
 
+
 				var newId = startId - 23;
 				var nextPosition = $('#'+newId);
 				if (nextPosition.hasClass('wall')){
@@ -46,6 +35,7 @@ $(document).keydown(function(e) {
 				currentPosition = nextPosition;
 				startId = newId;
 				}
+
         break;
 
         case 39: // right
@@ -71,6 +61,7 @@ $(document).keydown(function(e) {
 				} else {
 				$(nextPosition).addClass('box');
 				$(currentPosition).removeClass('box');
+				gameEnd();
 				currentPosition = nextPosition;
 				startId = newId;
 				}
@@ -78,5 +69,16 @@ $(document).keydown(function(e) {
 
         default: return; // exit for other keys
     }
-    e.preventDefault(); // prevent the default action (scroll / move caret)
+		// gameEnd();
+		e.preventDefault(); // prevent the default action (scroll / move caret)
 });
+
+function gameEnd() {
+	if (endId.hasClass('box')){
+		alert("END!");
+	}
+}
+
+function gameTime(){
+	
+}
